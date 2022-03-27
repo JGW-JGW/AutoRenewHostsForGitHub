e0318690ccaa41384ac1bceec7073c9ce728a347
jgw <jgw__jgw@163.com>
2022-03-27 10:34:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time    : 2022-03-27 10:29
+# Author  : Seto.Kaiba
+from pprint import pprint
+from typing import List, Dict
+import random as rd
+import math
+import datetime as dt
+import re
+from abc import ABCMeta, abstractmethod
+
+"""
+自动更新hosts，以更快地加载GitHub
+"""
+
+if __name__ == '__main__':
+    pass
